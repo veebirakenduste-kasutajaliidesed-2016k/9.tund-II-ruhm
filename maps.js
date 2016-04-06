24820deeb1f8f01fd1704c77695ae79bb34d1f80
romil rõbtšenkov <romilrobtsenkov@gmail.com>
2016-04-06 11:43:33 +0300

    added marker and infowindow

--- maps.js
+++ maps.js
@@ -29,13 +29,42 @@
 					lat: 59.439252, 
 					lng: 24.7721997
 				},
-				zoom: 6
-				
+				zoom: 6,
+				styles: [ { "elementType": "labels", "stylers": [ { "visibility": "off" } ] },{ "featureType": "water", "stylers": [ { "color": "#8080ed" } ] },{ "featureType": "road.highway", "stylers": [ { "hue": "#ff0022" } ] } ],
+				streetViewControl: false,
+				mapTypeControl: false
+							
 			};
 			
 			this.map = new google.maps.Map(this.container, options);
+			
+			this.map.addListener('click', function(e){
+				console.log(e.latLng.lat());
+				MapsApp.instance.createMarker(e.latLng.lat(), e.latLng.lng());
+			});
+			
 
         },
+		createMarker: function(newLat, newLng){
+			
+			var markerOptions = {
+				map: this.map,
+				position: {lat: newLat, lng: newLng},
+				animation: google.maps.Animation.DROP
+			};
+			
+			var newMarker = new google.maps.Marker(markerOptions);
+			
+			var infoOptions = {
+				content: "<strong>Tere</strong>"
+			};
+			
+			var infoWindow = new google.maps.InfoWindow(infoOptions);
+			
+			//seome markeriga
+			infoWindow.open(this.map, newMarker);
+			
+		}
     }; 
 
     window.onload = function(){
